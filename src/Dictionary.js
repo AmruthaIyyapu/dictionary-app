9f1b8159cf2da0db1e56742058787da79801172c
Amrutha Iyyapu <amrutha.iyyapu0909@gmail.com>
2023-04-21 19:41:21 -0400

    Integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,30 +1,75 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
-  let [word, setWord] = useState(null);
+  let [word, setWord] = useState();
+  let [loaded, setLoaded] = useState(false);
+  let [wordInfo, setWordInfo] = useState();
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [word]);
+
+  function handleResponse(response) {
+    // console.log(response.data.meanings[1].definition);
+
+    setWordInfo(response.data.meanings[1].definition);
+  }
 
   function search(event) {
     event.preventDefault();
-    alert(word);
+    setLoaded(true);
+    const apiKey = "908boa3025t814b4e4df14078410efc3";
+    // https://www.shecodes.io/learn/apis/dictionary
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${word}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleChange(event) {
     let userInputWord = event.target.value;
     setWord(userInputWord);
   }
-  return (
-    <div className="Dictionary container">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Enter a word"
-          autoFocus="ON"
-          className="w-75"
-          onChange={handleChange}
-        />
-        <input type="submit" value="SEARCH" className="m-5 btn btn-light p-3" />
-      </form>
-    </div>
-  );
+
+  if (loaded) {
+    return (
+      <div className="Dictionary container">
+        <form onSubmit={search}>
+          <input
+            type="search"
+            placeholder="Enter a word"
+            autoFocus="ON"
+            className="w-75"
+            onChange={handleChange}
+          />
+          <input
+            type="submit"
+            value="SEARCH"
+            className="m-5 btn btn-light p-3"
+          />
+        </form>
+        <h2>{word}</h2>
+        <div>{wordInfo}</div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <form onSubmit={search}>
+          <input
+            type="search"
+            placeholder="Enter a word"
+            autoFocus="ON"
+            className="w-75"
+            onChange={handleChange}
+          />
+          <input
+            type="submit"
+            value="SEARCH"
+            className="m-5 btn btn-light p-3"
+          />
+        </form>
+      </div>
+    );
+  }
 }
